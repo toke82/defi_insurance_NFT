7c579982085163d9e5eab32f7f8d7e90454ce5b0
adriancr1 <adrian.casal.rodriguez@gmail.com>
2023-09-29 18:37:32 +0200

    first commit

---
+++ src/USDCToken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract USDCToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("USDCToken", "USDC") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    } 
+}
